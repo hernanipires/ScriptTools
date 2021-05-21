e8922a8ef4b6c844fe0bfca5574a4d510877b4f6
hernanipires <58742353+hernanipires@users.noreply.github.com>
2021-05-21 18:33:26 -0400

    Update mountchk.sh
    Lectura del fstab

--- HPUX/mountchk.sh
+++ HPUX/mountchk.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/sh
 
-mount |awk '{print $1}'|sort -n > mount.date.last
+#Estrutura com uptime ultimo reinicio, gerar info antes do Reinicio, configurar crontab
+
+mount |awk '{print $1}'|sort -n > mount-$(date +%F%s).mnt
+
+
+grep -Ev "^#|^$" /etc/fstab |awk '{print $2}'|sort -n > mount-$(date +%F%s).mnt 
+
+diff $LAST1 $LAST2
